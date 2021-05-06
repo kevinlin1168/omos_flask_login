bf48f853302f7e60e9fea7c7469d9ca590316fda
HsuanYuLin <kevinlin1168@gmail.com>
2021-05-07 02:01:09 +0800

    first commit

---
+++ app/static/js/fb_sdk.js
@@ -0,0 +1,24 @@
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+
+    FB.init({
+        appId      : '134414618673707',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v10.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+};
